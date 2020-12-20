48d5b7d6bc611f21d3b386b1aaac897f3f9c95e1
Jarkko Toivonen <jttoivon@cs.helsinki.fi>
2020-12-20 23:25:53 +0200

    Remove polls app

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,14 +93,14 @@ LOGGING = {
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
-    'django.contrib.admin',
+     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes.apps.NotesConfig',
+#   'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
